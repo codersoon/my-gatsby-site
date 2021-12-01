92e0a017d57a6fff5fc3e5f7dcc711bcaef061c7
codersoon <ksh1210sh@naver.com>
2021-12-01 13:50:58 +0900

    text-add

--- src/pages/index.js
+++ src/pages/index.js
@@ -139,7 +139,7 @@ const IndexPage = () => {
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        Congratulations
+        Congratulations (Commit Testing !!!)
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
         <span role="img" aria-label="Party popper emojis">
