414b9557fe6479e93033fbdaa6847c1118d37e49
codersoon <ksh1210sh@naver.com>
2021-12-13 14:41:37 +0900

    newsite-testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -140,7 +140,7 @@ const IndexPage = () => {
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        Congratulations (Commit Testing !!!)asas
+        Congratulations (Commit Testing !!!)
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
         <span role="img" aria-label="Party popper emojis">
